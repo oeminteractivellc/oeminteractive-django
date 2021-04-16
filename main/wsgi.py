4e11d66e8044d5614045a2e5f672e122e7bc478d
James Echmalian <ech@ech.net>
2021-04-16 16:22:10 -0400

    Modify wsgi.py for deploy to Lightsail.

--- main/wsgi.py
+++ main/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Put this directory and the parent onto the path, for running via Apache WSGI.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
