cb7c85fe20646380670baedcfcad7e5b86386cf0
James Echmalian <ech@ech.net>
2021-06-04 13:18:48 -0400

    More page builder fixes.

--- static_dev/js/local.js
+++ static_dev/js/local.js
@@ -216,7 +216,7 @@ function loadPageConfiguration(pageKey, contentIndex) {
     }
     empty = false;
     forEachContentSection(function(section) {
-      if (!section.group || keepAllSections) {
+      if (section.group !== 'deluxe' || keepAllSections) {
         addSection(section);
       }
     })
