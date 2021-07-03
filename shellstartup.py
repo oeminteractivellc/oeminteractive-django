636550f597862ce261e9a12f6fef2173a0364756
James Echmalian <ech@ech.net>
2021-07-03 12:25:39 -0400

    Add shellstartup.py

---
+++ shellstartup.py
@@ -0,0 +1,6 @@
+from content.models import *
+from core.models import *
+from companalysis.models import *
+from companalysis.queries import *
+from companalysis.services import *
+from companalysis.tasks import *
