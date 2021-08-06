9126eaae269ede40a12027f564bf363ba15fda0e
James Echmalian <ech@ech.net>
2021-08-06 18:39:36 -0400

    Show tags not filename in media search results.

--- media/api/serializers.py
+++ media/api/serializers.py
@@ -6,10 +6,14 @@ from media.models import MediaFile
 
 class MediaFileSerializer(serializers.ModelSerializer):
   url = serializers.SerializerMethodField()
+  tags = serializers.SerializerMethodField()
 
   class Meta:
     model = MediaFile
-    fields = ("id", "url", "created_at")
+    fields = ("id", "url", "created_at", "tags")
 
   def get_url(self, obj):
     return f"{settings.MEDIA_SITE_URL}/media/m/{obj.id}"
+
+  def get_tags(self, obj):
+    return {tag.key: tag.value for tag in obj.tags.all()}
